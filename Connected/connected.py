d9b95b4a772a523742b16beb0a3dc51fc8e34e99
Dinis <dinispeixoto96@gmail.com>
2018-03-09 14:05:55 +0000

    iterating --n_iterations for each number of nodes

--- Connected/connected.py
+++ Connected/connected.py
@@ -28,25 +28,25 @@ def generate_graph(n_vertex):
 	return Graph
 
 def generate_plot():
-	vertex_edges = [(n_vertex, generate_graph(n_vertex).number_of_edges()) 
-		for n_vertex in [x * args.vertex for x in range(1,args.iterations+1)]]
+	vertex_edges = [(n_vertex, sum([generate_graph(n_vertex).number_of_edges() for x in range(args.n_iterations)])/args.n_iterations)
+		for n_vertex in range(args.initial_nodes, args.final_nodes, args.step)]			
 
 	[x_axis, y_axis] = list(zip(*vertex_edges))
 	draw_plot(x_axis, y_axis)
 
-
 if __name__== "__main__":
 	parser = argparse.ArgumentParser(prog='connected')
-	parser.add_argument('-v','--vertex',help = 'number of initial vertex', type=int, default=10)
-	parser.add_argument('-i','--iterations', help='number of iterations', type=int, default=10)
-	parser.add_argument('-o', '--output', help='output file', required=True)
-	parser.add_argument('-p','--plot',help='generates a plot with the average number of random edges necessary to completly connect a graph', action="store_true")
+	parser.add_argument('-i','--initial_nodes', help = 'number of initial nodes', type = int, default = 10)
+	parser.add_argument('-s','--step', help = 'step for next number of nodes', type = int, default = 10)
+	parser.add_argument('-f','--final_nodes', help = 'number of final nodes', type = int, default = 110)
+	parser.add_argument('-n','--n_iterations', help ='number of iterations for each node', type = int, default = 10)
+	parser.add_argument('-o', '--output', help = 'output file', required = True)
+	parser.add_argument('-p','--plot', help ='generates a plot with the average number of random edges necessary to completly connect a graph', action="store_true")
 
 	args = parser.parse_args()
 
 	if args.plot:
 		generate_plot()
 	else:
-		graph = generate_graph(args.vertex)
+		graph = generate_graph(args.initial_nodes)
 		draw_graph(graph)
-	
