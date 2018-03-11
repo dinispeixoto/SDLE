5ba8d67c64f3fb237ba61de07956cdf3acc515e1
Dinis <dinispeixoto96@gmail.com>
2018-03-11 23:24:03 +0000

    have no ideia if its right or not

--- Preferencial/preferencial.py
+++ Preferencial/preferencial.py
@@ -4,8 +4,20 @@ import random
 import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import Counter
 
 from sys import argv
+
+def draw_graph(Graph):
+    labels = nx.get_node_attributes(Graph, 'weight')
+    nx.draw(Graph, labels = labels)
+    plt.savefig(args.output)
+
+def draw_plot(x_axis, y_axis):
+    plt.ylabel('Degree')
+    plt.xlabel('Nodes')
+    plt.plot(x_axis, y_axis, marker='.')
+    plt.savefig(args.output)
 
 def generate_preferencial_graph(n_vertex):
     # init graph
@@ -19,23 +31,29 @@ def generate_preferencial_graph(n_vertex):
         Graph.add_edge(x,y)
         Graph.node[x]['weight'] += 1 
         Graph.node[y]['weight'] += 1
-
     return Graph 
 
-def draw_graph(Graph):
-    labels = nx.get_node_attributes(Graph, 'weight')
-    nx.draw(Graph, labels = labels)
-    plt.savefig('this.png')
-    plt.show() 
+def generate_plot():
+    degrees = [degree for (node, degree) in generate_preferencial_graph(args.nodes).degree()]
+    #degree_nodes = Counter(degrees)
+    degree_nodes = { degree:sum(i >= degree for i in degrees) for degree in degrees}
+    
+    for k, v in degree_nodes.items():
+        print(k,v)
+    
+    y_axis, x_axis = zip(*degree_nodes.items())    
+    draw_plot(x_axis, y_axis)    
 
 if __name__== "__main__":
     parser = argparse.ArgumentParser(prog='preferencial')
-    parser.add_argument('-v','--vertex',help = 'number of initial vertex', type=int, default=10)
     parser.add_argument('-o', '--output', help='output file', required=True)
     parser.add_argument('-p','--plot',help='generates a plot', action="store_true")
+    parser.add_argument('-n','--nodes', help = 'number of initial nodes', type = int, default = 10)
 
     args = parser.parse_args()
 
-    if not args.plot:
-        Graph = generate_preferencial_graph(args.vertex)
+    if args.plot:
+        generate_plot()
+    else:
+        Graph = generate_preferencial_graph(args.nodes)
         draw_graph(Graph)
