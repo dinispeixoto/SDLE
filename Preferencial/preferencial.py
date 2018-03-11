e13e791a4e26ae16df6c8db4916d95021f99af39
Dinis <dinispeixoto96@gmail.com>
2018-03-11 23:40:05 +0000

    done

--- Preferencial/preferencial.py
+++ Preferencial/preferencial.py
@@ -20,27 +20,21 @@ def draw_plot(x_axis, y_axis):
     plt.savefig(args.output)
 
 def generate_preferencial_graph(n_vertex):
-    # init graph
     Graph = nx.Graph()
     Graph.add_nodes_from(range(n_vertex), weight = 1)
 
-    # adding preferencial edges
     while not nx.is_connected(Graph):
         probability_list = sum([[node] * Graph.node[node]['weight'] for node in Graph.nodes],[]) 
         [x,y] = random.sample(probability_list, 2)
         Graph.add_edge(x,y)
         Graph.node[x]['weight'] += 1 
         Graph.node[y]['weight'] += 1
+        
     return Graph 
 
 def generate_plot():
     degrees = [degree for (node, degree) in generate_preferencial_graph(args.nodes).degree()]
-    #degree_nodes = Counter(degrees)
     degree_nodes = { degree:sum(i >= degree for i in degrees) for degree in degrees}
-    
-    for k, v in degree_nodes.items():
-        print(k,v)
-    
     x_axis, y_axis = zip(*degree_nodes.items())    
     draw_plot(x_axis, y_axis)    
 
